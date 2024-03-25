ad3a8acd0e60c4ca0373bc6b8acabf6ec70cc6f5
Ganesh Chandra Bisoi <ganesh999.gcb@gmail.com>
2024-03-25 08:30:14 +0530

    All other files uploaded

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+// $(function () {
+//     var overlay = $('<div id="overlay"></div>');
+//     overlay.show();
+//     overlay.appendTo(document.body);
+//     $('.popup').show();
+//     $('.popup').hide(20000);
+// });
